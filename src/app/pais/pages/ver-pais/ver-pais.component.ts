b5d9f50677480dbfe5d1a3915b59dad4b202246d
Eri-random <brenherrera@alumno.unlam.edu.ar>
2023-02-09 20:50:43 -0300

    add ver pais

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -7,5 +11,34 @@ import { Component } from '@angular/core';
   ]
 })
 export class VerPaisComponent {
+
+  pais!: Country;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private PaisService:PaisService
+  ){}
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params
+    .pipe(
+      switchMap(( param ) =>
+        this.PaisService.getPaisPorAlpha( param['id'])),
+        tap(console.log)
+    )
+    .subscribe(pais => this.pais = pais);
+
+  /*this.activatedRoute.params
+   .subscribe( ({id}) =>{
+    console.log(id);
+
+    this.PaisService.getPaisPorAlpha(id)
+    .subscribe(pais=>{
+      console.log(pais);
+    })
+   })*/
+    
+  }
 
 }
