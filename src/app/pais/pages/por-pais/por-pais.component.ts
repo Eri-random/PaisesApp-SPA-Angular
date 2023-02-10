01d7af820f8e395f1774adf67d8df796cf6828e1
Eri-random <brenherrera@alumno.unlam.edu.ar>
2023-02-10 17:04:21 -0300

    add sugerencias

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+    li{
+      cursor: pointer;
+    }
+    `
   ]
 })
 export class PorPaisComponent {
@@ -13,6 +18,9 @@ export class PorPaisComponent {
   termino: string =''
   hayError: boolean= false;
   paises: Country[]=[];
+  paisesSugeridos :Country[]=[];
+
+  mostrarSugerencias : boolean = false;
 
   constructor(private paisService: PaisService){
 
@@ -21,6 +29,8 @@ export class PorPaisComponent {
   buscar( termino: string){
     this.hayError=false;
     this.termino = termino;
+    this.mostrarSugerencias= false;
+
     
     this.paisService.buscarPais(this.termino)
     .subscribe(paises => {
@@ -33,8 +43,19 @@ export class PorPaisComponent {
 
   sugerencias(termino: string){
     this.hayError=false;
+    this.termino = termino;
+    this.mostrarSugerencias=true;
     
     //TODO: crear sugerencias
+    this.paisService.buscarPais(termino)
+    .subscribe(
+      paises => this.paisesSugeridos = paises.splice(0,5),
+      (err) => this.paisesSugeridos = []
+    )
+  }
+
+  buscarSugerido(termino : string){
+    this.buscar(termino);
   }
 
 }
